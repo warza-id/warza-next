685adecbf098db3028424dda53b9e516e97e65b9
Kiki Kiswanto <kiki.kiswanto22@gmail.com>
2025-10-17 07:19:39 +0700

    [APP] Add Brevo Chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Warza - Jasa Pembuatan Software dan IT Konsultan',
@@ -12,9 +13,28 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const brevoScript = `
+    (function(d, w, c) {
+      w.BrevoConversationsID = '68a11d06cc6cb9aa7c089999';
+      w[c] = w[c] || function() {
+        (w[c].q = w[c].q || []).push(arguments);
+      };
+      var s = d.createElement('script');
+      s.async = true;
+      s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+      if (d.head) d.head.appendChild(s);
+    })(document, window, 'BrevoConversations');
+  `;
+
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <Script
+          id="brevo-conversations-script" // ID unik
+          dangerouslySetInnerHTML={{ __html: brevoScript }}
+          strategy="lazyOnload" // Strategi pemuatan
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
